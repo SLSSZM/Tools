c11f4a2179cf7350f2077531cd30a1a57d2d67a4
i0CT <211722662@qq.com>
2021-05-24 18:46:15 +0800

    添加注释

--- get_favicon/index.js
+++ get_favicon/index.js
@@ -32,14 +32,19 @@ request({ url: path + '/favicon.ico', timeout: 500 }, function (error, response,
 })
 
 function filter (data) {
+    // 用cheerio解析网址源码
     let $ = cheerio.load(data)
+    // 获取link标签
     let t = $('link')
+    // 用正则对/favicon.ico和shortcut icon进行判断
     let patt = /^\/favicon\./i
     let icon = /(shortcut|icon)/i
     for (let i = 0; i < t.length; i++) {
+        // 查到与icon正则相同的就获取它的href
         if (icon.test(t[i].attribs.rel)) {
+            // 再次判断href路径是否为/favicon.ico
             if (patt.test(t[i].attribs.href)) {
-                return console.log(a + t[i].attribs.href)
+                return console.log(patt + t[i].attribs.href)
             }
             return console.log(t[i].attribs.href)
         }
